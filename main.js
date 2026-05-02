550905f043129fb9fe3ec94e8aaca8e139761eac
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-05-02 11:13:45 -0500

    Add task adding and showing
    Added task creation, stored tasks in an array, implemented renderTasks(), fixed script connection (app.js → main.js). Users can now add and view tasks dynamically.

---
+++ main.js
@@ -0,0 +1,44 @@
+let tasks = []
+// First get stuff from index.html
+const taskInput = document.getElementById('taskInput')
+const taskList = document.getElementById('taskList')
+const addBtn = document.getElementById('addBtn')
+
+// When button is clicked 
+addBtn.addEventListener('click', addTask)
+
+// Also allow pressing Enter key, this is a litttle upgrade
+taskInput.addEventListener('keypress', function (e) {
+    
+    if (e.key === 'Enter') {
+        addTask()
+ }
+})
+
+// with a python background i did this "function addTask(taskText)" 😅
+function addTask() {
+    const taskText = taskInput.value.trim() // get input value
+
+    if (taskText) {
+        const task = {
+            id: Date.now(),
+            text: taskText,
+            completed: false
+        }
+
+        tasks.push(task) // store in array
+        renderTasks() // show on screen
+        taskInput.value = '' // clear input
+    }
+}
+
+function renderTasks() {
+    taskList.innerHTML = '' // clear list first
+
+    tasks.forEach(function(task) {
+        const li = document.createElement('li') // create a new line, so it will also be easier to addd checkboxes
+        li.textContent = task.text // put text inside
+
+        taskList.appendChild(li) // add to the list
+    })
+}
